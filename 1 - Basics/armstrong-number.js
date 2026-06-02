33c5377b4a572a0810101b545e67e9f62af93c46
Diksha Sobhawat <dikshasobhawat231@gmail.com>
2026-06-02 20:18:38 +0530

    armstrong-number-checker

---
+++ 1 - Basics/armstrong-number.js
@@ -0,0 +1,14 @@
+let num = 153;
+let original = num;
+let sum = 0;
+while(num>0){
+    let digit = num%10;
+    num = Math.floor(num/10);
+    sum=sum+Math.pow(digit,3);
+} 
+if(sum==original){
+    console.log("Armstrong number");
+}
+else{
+    console.log("Not a armstrong number");
+}
