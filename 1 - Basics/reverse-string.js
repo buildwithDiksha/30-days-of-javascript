a7bc61ed318d05969be150baa93b9af93b593254
Diksha Sobhawat <dikshasobhawat231@gmail.com>
2026-06-02 23:21:15 +0530

    to reverse a string

---
+++ 1 - Basics/reverse-string.js
@@ -0,0 +1,5 @@
+let str = "diksha";
+let reverse = str.split("").reverse().join(""); 
+console.log(reverse);
+
+// phle str ko split krega jis se vo array bn jaega ["d","i","k","s","h","a"] the isko reverse krega and reverse krne k bad vo aaray split hua usko vapis join krdega 
