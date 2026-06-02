1dea84c80af0b6ecb3f4cbabd02892fd5f2f9296
Diksha Sobhawat <dikshasobhawat231@gmail.com>
2026-06-02 13:30:37 +0530

    Added largest-of-three numbers

---
+++ 1 - Basics/largest-of-three.js
@@ -0,0 +1,12 @@
+let num1=7;
+let num2=2;
+let num3=1;
+if((num1>num2) && (num1>num3)){
+    console.log("Number 1 is greater");
+}
+else if((num2>num1) && (num2>num3)){
+    console.log("Number 2 is greater");
+}
+else{
+    console.log("Number 3 is greater");
+}
