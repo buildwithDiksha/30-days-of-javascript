66a053cbab0b9402799a58df38134c517871600d
Diksha Sobhawat <dikshasobhawat231@gmail.com>
2026-06-02 23:35:38 +0530

    swap two numbers without using third variable

---
+++ 1 - Basics/swap-numbers.js
@@ -0,0 +1,7 @@
+let a = 5;
+let b = 10;
+
+a=a+b; // a=5+10  15
+b=a-b;    //now the value will get change a =15 ho jaega to ab hoga 15-10=5 ab b =5 hogya
+a=a-b  // 15-5 = 10 
+console.log("a =",a ,"b =",b);
