81578bde97135460f2890ccf500f5e3900adf9f3
Diksha Sobhawat <dikshasobhawat231@gmail.com>
2026-06-03 18:24:40 +0530

    words in a sentence

---
+++ 1 - Basics/words-in-a-sentence.js
@@ -0,0 +1,7 @@
+let sentence = "My name is Diksha";
+let words= sentence.split(" ");   
+let wordCount = words.length;
+console.log(wordCount)
+
+// "" - ye string ko characters k base pr split krta h mtlb 'm''y' 'n''a''m' 'e' aise split hoga
+// aur agr space k sath likhe to " " words k base pr split krta h 'my' 'name'
