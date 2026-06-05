70ff938f65a2522a51812b997a1788b9b4fac724
Diksha Sobhawat <dikshasobhawat231@gmail.com>
2026-06-05 11:55:38 +0530

    array method

---
+++ 2 - Arrays/array-methods2.js
@@ -0,0 +1,40 @@
+// 5 - Reverse
+let arr = [1,2,3,4];
+arr.reverse();
+console.log(arr);
+
+
+
+
+
+// 6 - Sorting 
+let fruits =["orange","apple","mango","banana","watermelon"];
+fruits.sort();
+console.log(fruits);
+
+
+let  arr1 = [2,1,3,5]
+arr1.sort((a,b)=>a-b);   // ascending order
+console.log(arr1);
+
+let arr2 = [0,4,2,5,6,9,8]
+arr2.sort((a,b)=>b-a) // descending order
+console.log(arr2);
+
+let arr3 = [9,2,3,4,1,7]
+arr3.sort()
+console.log(arr3);
+
+
+
+
+
+
+
+
+
+// 7- 
+// join -  Convert Array to String
+let name = ["diksha","manisha","nisha","anshul"]
+name.join(" ")
+console.log(name);
