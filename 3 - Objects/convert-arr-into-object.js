58cf01403ad86e7a294103b1ff4bda9c7f594a95
Diksha Sobhawat <dikshasobhawat231@gmail.com>
2026-06-10 17:34:11 +0530

    convert array into object

---
+++ 3 - Objects/convert-arr-into-object.js
@@ -0,0 +1,8 @@
+let arr = [["a","name"],["b",20],["c","age"]]
+let result = Object.fromEntries(arr)
+console.log(result);
+
+
+
+// Array ko object me convert karne ke liye hum Object.fromEntries() use karte hain.
+// Iske liye array ke andar [key, value] pairs hone chahiye
