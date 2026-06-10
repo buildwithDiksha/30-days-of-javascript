171905e86d19c6bfb63a52e3844e7e63f0995dc1
Diksha Sobhawat <dikshasobhawat231@gmail.com>
2026-06-10 12:11:46 +0530

    Print key,values and properties of a object

---
+++ 3 - Objects/print-key-values-properties-in-object.js
@@ -0,0 +1,11 @@
+let student = {
+    name : "diksha",
+    age : 20,
+    course : "Btech",
+    duration : "4 years"
+}
+console.log(Object.keys(student));
+console.log(Object.values(student));
+console.log(Object.entries(student));
+
+
